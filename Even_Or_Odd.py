cb5c9aadd56f423dc836a5b51fa9df2a8c4277fc
Aarthi.S <168005532+Aarthi-2004@users.noreply.github.com>
2025-01-29 18:44:29 +0530

    Even_Or_Odd.py

---
+++ Even_Or_Odd.py
@@ -0,0 +1,6 @@
+#Question to check if a Number is Odd or Even
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
