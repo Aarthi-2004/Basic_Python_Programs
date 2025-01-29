a41e05e5891912a106d7d7197758e4bbc5ec2347
Aarthi.S <168005532+Aarthi-2004@users.noreply.github.com>
2025-01-29 18:59:21 +0530

    Multiplication_table.py

---
+++ Multiplication_table.py
@@ -0,0 +1,7 @@
+#Question for multiplication table:
+def multiplicationTable(N):
+    for i in range(1,11): 
+        print(i * N, end=" ")
+    print("\n")
+multiplicationTable(5)
+multiplicationTable(6)
