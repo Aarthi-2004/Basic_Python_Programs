bf35734e3136853aeff6476be108c91dd8530b8d
Aarthi.S <168005532+Aarthi-2004@users.noreply.github.com>
2025-01-29 18:42:49 +0530

    Positive_Negative_Numbers.py

---
+++ Positive_Negative_Numbers.py
@@ -0,0 +1,19 @@
+#Question to find to Check if a Number is Positive, Negative or 0
+Using if...elif...else:
+num = float(input("Enter a number: "))
+if num > 0:
+   print("Positive number")
+elif num == 0:
+   print("Zero")
+else:
+   print("Negative number")
+
+Using Nested if:
+num = float(input("Enter a number: "))
+if num >= 0:
+   if num == 0:
+       print("Zero")
+   else:
+       print("Positive number")
+else:
+   print("Negative number")
